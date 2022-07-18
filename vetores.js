0b920c15ed8ff147e823a3d7b0932109b93c848a
Prezotti <henriqueprezotti@gmail.com>
2022-07-18 20:05:56 -0300

    Estilização da página de Pesquisa

--- vetores.js
+++ vetores.js
@@ -19,7 +19,11 @@ function carregarImoveis(filtroArea, tipoImovel, filtroCidade, filtroBairro){
 
     //cidade = cidade.sort(); //Ordena o vetor cidade em ordem alfabética
 
+    var datalist = document.getElementById("cidadesDisponiveis");
+    datalist.innerText ="";
+
     //Cria um vetor com as cidades disponíveis
+
     var cidadesDisponiveis = [];
     for(var i = 0; i < cidade.length; i++){
         if (cidadesDisponiveis.indexOf(cidade[i]) == -1){
@@ -30,7 +34,7 @@ function carregarImoveis(filtroArea, tipoImovel, filtroCidade, filtroBairro){
 
 
     for(let i = 0; i < cidadesDisponiveis.length; i++){
-        var datalist = document.getElementById("cidadesDisponiveis");
+        datalist = document.getElementById("cidadesDisponiveis");
         var option = document.createElement("option");
         option.value = cidadesDisponiveis[i];
         datalist.appendChild(option);
@@ -69,12 +73,15 @@ function carregarImoveis(filtroArea, tipoImovel, filtroCidade, filtroBairro){
 
         areaCImovel.textContent = "Área contruida: " + areaC[i] + "m²";
         areaCImovel.classList.add("area-construida-imovel")
+        areaCImovel.classList.add("tipo-detalhes");
 
         areaTImovel.textContent = "Área total: " + areaT[i] + "m²";
-        areaCImovel.classList.add("area-total-imovel");
+        areaTImovel.classList.add("area-total-imovel");
+        areaTImovel.classList.add("tipo-detalhes");
 
         enderecoImovel.textContent = logradouro[i] + ", " + bairro[i] + ", CEP: " + CEP[i];
         enderecoImovel.classList.add("endereco-imovel");
+        enderecoImovel.classList.add("tipo-detalhes");
 
         informacoesImovel.appendChild(tituloImovel)
         informacoesImovel.appendChild(areaCImovel)
@@ -152,29 +159,38 @@ function conferirBairros(){
     option.textContent = "Todos";
     select.appendChild(option);
 
-    //Cria um vetor só com os bairros disponíveis dependendo da cidade selecionada
     var bairrosDisponiveis = [];
+
     for(var i = 0; i < bairro.length; i++){
-        if (bairrosDisponiveis.indexOf(bairro[i]) == -1){
-            bairrosDisponiveis.push(bairro[i]);
-        }
-    }
-    bairrosDisponiveis = bairrosDisponiveis.sort();
-
-    for(var i = 0; i < bairrosDisponiveis.length; i++){
-                                //A primeira letra maiúscula concatenada com uma substring a partir do índice 1 toda lowercase
-        bairrosDisponiveis[i] = bairrosDisponiveis[i][0].toUpperCase() + bairrosDisponiveis[i].substring(1).toLowerCase();
-    }
-
-    for(var i = 0; i < bairrosDisponiveis.length; i++){
-        if(cidadeSelecionada == cidade[i] || cidadeSelecionada == ""){
-            var option = document.createElement("option");
-            option.value = bairrosDisponiveis[i];
-            option.textContent = bairrosDisponiveis[i];
-            select.appendChild(option);
+        if(cidadeSelecionada == cidade[i] || cidadeSelecionada == ""){ //Confere se o bairro corresponde a cidade
+            if (bairrosDisponiveis.indexOf(bairro[i]) == -1){ //confere se esse bairro já foi adicionado
+                bairrosDisponiveis.push(bairro[i]); //Adiciona esse bairro no vetor
+                var option = document.createElement("option");
+                option.value = bairro[i];
+                option.textContent = bairro[i][0].toUpperCase() + bairro[i].substring(1).toLowerCase(); //Mostra o texto com a primeira letra maiúscula
+                select.appendChild(option);
+            }
+            
         }
     }
 
+}
+
+function limparFiltros(){
+    inCidade.value = "";
+    inBairro.value = "Todos";
+    
+    cbApartamento.checked = false;
+    cbCasa.checked = false;
+    cbSitio.checked = false;
+    cbLoja.checked = false;
+    cbDuplex.checked = false;
+    cbTerreno.checked = false;
+
+    inAreaMinima.value = 0;
+    inAreaMaxima.value = "";
+
+    carregarImoveis([0, 0], [], 'Todas', 'Todos');
 }
 
 btFiltrar.addEventListener("click", filtrarImoveis);
