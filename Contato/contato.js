4b395bd592222d88ee247e769bb4e94321d62672
Paulocezarbs <paulocezarborges175@gmail.com>
2022-07-27 08:14:06 -0300

    finalização pagina contato

--- Contato/contato.js
+++ Contato/contato.js
@@ -1,9 +1,10 @@
 var rbOfertas = document.getElementById("receberOfertas");
 var formOfertas = document.querySelector(".form-ofertas");
+formOfertas.style.display = "none"
 
 // Function para fazer aparecer e desaparecer a area de ofertas na pagina contato
 function mostrarArea() {
-
+console.log(rbOfertas.checked)
   if (rbOfertas.checked) {  
     formOfertas.style.display = "block"
   } else {
